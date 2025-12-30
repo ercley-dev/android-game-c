bac4d7f06b10c3f206ac4a9a1eff5d184187f05d
Ercley Dev <erison.cleyton@engineer.com>
2025-12-29 23:03:39 -0300

    Workflow completo e corrigido para 'build' app

---
+++ app/src/main/java/org/libsdl/app/MainActivity.java
@@ -0,0 +1,13 @@
+package org.libsdl.app;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "main"
+        };
+    }
+}
